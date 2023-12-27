6a64af69426485d65b55227ebac128c443ddac55
Juansantoss07 <juansantosscunha@gmail.com>
2023-12-27 15:42:54 -0300

    mais sobre métodos (métodos mágicos, e mebros estáticos)

--- Orientação_a_Objetos/Conta.php
+++ Orientação_a_Objetos/Conta.php
@@ -7,15 +7,26 @@ class Conta // Aqui criamos uma classe, uma classe pode ser um tipo também.
     // Note que temos propriedades public e private, isso serve para decidirmos se nossa propriedade vai poder ser acessada fora do arquivo ou somente dentro do arquivo. No caso da propriedade $saldo por exemplo, não podemos deixa-la como pública para que não ocorra o risco de alteração no valor do saldo em outro lugar do código fora desse arquivo.
     private string $cpfTitular;
     private string $nomeTitular;
-    private float $saldo = 0; // Aaui estamos atribuindo o valor 0 para saldo, assim sempre que criarmos uma conta nova, o saldo já virá como 0. 
-
+    private float $saldo; 
     // Todas essas prioridades são variáveis que chamamos de atributos da nossa classe
-    
+    private static $numeroDeContasCriadas = 0;
 
     public function __construct(string $cpf, string $nome)
     {
         $this->cpfTitular = $cpf;
         $this->nomeTitular = $nome;
+        $this->saldo = 0; // Aaui estamos atribuindo o valor 0 para saldo, assim sempre que criarmos uma conta nova, o saldo já virá como 0. 
+        $this->validaNomeTitular($nome);
+        
+
+        self::$numeroDeContasCriadas++;
+        echo self::retornarNumeroDeContasCriadas();
+    }
+
+    public function __destruct()
+    {
+        self::$numeroDeContasCriadas--;
+        // Com esse método mágico, sempre que não tivermos mais o objeto em questão, vai ser executado o código que inserirmos aqui.
     }
 
     public function sacar(float $valor)
@@ -63,5 +74,19 @@ class Conta // Aqui criamos uma classe, uma classe pode ser um tipo também.
     public function retornarCpfTitular()
     {
         return $this->cpfTitular;
+    }
+
+
+    private function validaNomeTitular($nome)
+    {
+        if(strlen($nome) <= 5){
+            echo "O nome precisa ter ao menos 6 caracteres.";
+            exit();
+        }
+    }
+
+    private static function retornarNumeroDeContasCriadas()
+    {
+        return self::$numeroDeContasCriadas;
     }
 }
